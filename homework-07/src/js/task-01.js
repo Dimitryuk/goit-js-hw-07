ead50142176ed3b56fa0f58e2f3a8d6805170542
Alexander Dimitryuk <dimitryuk38@gmail.com>
2021-07-07 11:41:47 +0300

    2 задание сделано

---
+++ homework-07/src/js/task-01.js
@@ -0,0 +1,5 @@
+const categoriesCount = document.querySelectorAll('.item')
+console.log(`В списке ${categoriesCount.length} категории.`);
+
+const listEl = document.querySelector('h2')
+console.log(listEl.textContent);
--- homework-07/src/js/task-02.js
+++ homework-07/src/js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+// const ingredientsEl = document.createElement('li')
+// ingredientsEl.textContent = ingredients[0]
+// console.log(ingredientsEl.textContent = ingredients[3]);
+
+// ingredients.forEach(ingredient=>)
+const elements =[]
+for (let i = 0; i < ingredients.length; i += 1){
+  const listEl = document.createElement('li')
+listEl.textContent = ingredients[i]
+   elements.push(listEl)
+  console.log(elements);
+
+}
+
+const ingredientsList = document.querySelector('#ingredients')
+// console.log(ingredientsList);
+ingredientsList.append(...elements)
