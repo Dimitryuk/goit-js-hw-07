95e5970bc038718d7b0657d406efc0ce8a2565eb
Alexander Dimitryuk <dimitryuk38@gmail.com>
2021-07-13 15:25:41 +0300

    5 закончил

--- homework-07/src/js/task-05.js
+++ homework-07/src/js/task-05.js
@@ -7,18 +7,11 @@ console.log(refs.input);
 refs.input.addEventListener('input', onInputChange)
 
 function onInputChange() {
-    console.log(event.currentTarget.value);
-    refs.output.textContent = event.currentTarget.value
-    if (refs.input = '') {
-      refs.output.textContent = 'незнакомец'   
+    if (refs.input.value !== '') {
+        refs.output.textContent = event.currentTarget.value
+    } else {
+        refs.output.textContent = "незнакомец"
     }
 }
 
-// refs.input.addEventListener('blur', onInputBlur)
-// function onInputBlur() {
-//     refs.output.textContent='незнакомец'
-//     //  if (refs.input = '') {
-//     //   refs.output.textContent = 'незнакомец'   
-//     // }
-//     console.log('потерял фокус');
-// }
+
